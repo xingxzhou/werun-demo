f744bdc5fcdc43bc4464a4ae8cc4b7e702478aac
Patrick <zdxf@qq.com>
2025-10-27 18:24:32 +0800

    update

--- database/models/exhibition.js
+++ database/models/exhibition.js
@@ -1,45 +1,49 @@
-const { sequelize } = require("../sequelize");
+const { sequelize } = require('../sequelize');
 const { DataTypes } = require('sequelize');
 
 const Exhibition = sequelize.define(
-	'Exhibition',
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true, // 自动递增
-			allowNull: false,
-		},
-		title: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-			defaultValue: '',
-		},
-		description: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-		},
-		start_date: {
-			type: DataTypes.DATEONLY, // DATE 类型
-			allowNull: false,
-		},
-		end_date: {
-			type: DataTypes.DATEONLY,
-			allowNull: false,
-		},
-		cover_image: {
-			type: DataTypes.STRING(191),
-			allowNull: false,
-			defaultValue: '',
-		},
-	},
-	{
-		tableName: 'Exhibitions', // 指定数据库表名
-		timestamps: false, // 如果你没有 createdAt / updatedAt
-		comment: '展览表', // 表注释
-	}
+  'Exhibition',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, // 自动递增
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING(191),
+      allowNull: false,
+      defaultValue: '',
+    },
+    title_rich: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    description_rich: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    start_date: {
+      type: DataTypes.DATEONLY, // DATE 类型
+      allowNull: false,
+    },
+    end_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    cover_image: {
+      type: DataTypes.STRING(191),
+      allowNull: false,
+      defaultValue: '',
+    },
+  },
+  {
+    tableName: 'Exhibitions', // 指定数据库表名
+    timestamps: false, // 如果你没有 createdAt / updatedAt
+    comment: '展览表', // 表注释
+  }
 );
 
 module.exports = {
-	Exhibition
-}
+  Exhibition,
+};
